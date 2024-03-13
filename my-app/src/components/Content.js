d3b85d927aa7b1c5d85735498d1d6f097dc0c84f
Judit3 3 <katapunpinpun@yahoo.es>
2024-03-13 12:15:28 +0000

    add a loadder image with componentdidmount

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react'
-import {savedPosts} from '../posts.json'
-import css from './css/Content.module.css'
-import PostItem from './PostItem'
+import React, { Component } from 'react';
+import {savedPosts} from '../posts.json';
+import css from './css/Content.module.css';
+import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
-      super(props)
+        super(props)
+        this.state= {
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
     }
 
     render() {
         return (
-        <div className={css.Content}>Content
-            <div className={css.TitleBar}>
-                <h1>My Photos</h1>
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
+
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
+                </div>
             </div>
-            <div className={css.SearchResults}>
-                {/*{savedPosts.map((post)=>{
-                    return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img scr={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                            </div>
-                    }   
-                )}*/}  
-                <PostItem savedPosts={savedPosts}/>
-            </div>
-        </div>
         )
     }
 }
